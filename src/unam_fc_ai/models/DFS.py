0ab5d9787c1261d686a59605fc06c8e4fad30454
Tocny <marcoarubior@ciencias.unam.mx>
2026-06-08 21:39:47 -0600

    New structure

--- src/unam_fc_ai/models/DFS.py
+++ src/unam_fc_ai/models/DFS.py
@@ -5,7 +5,7 @@ Facultad de Ciencias
 Materia: Inteligencia Artificial
 Docente: Dra. Jessica Sarahi Méndez Rincón
 Ayudante de Laboratorio: Diego Eduardo Peña Villegas
-Alumno: Escobar González Isaac Giovani
+Alumno: Rubio Resendiz Marco Antonio
 Año escolar: 2026-2
 Copyright: (c) 2025 UNAM - MIT License
 This software is for educational purposes.  
